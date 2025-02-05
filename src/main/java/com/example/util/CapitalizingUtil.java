79999e549b9a72364074358e29aa8acd2e36be0e
merge-simpson <letsdev@kakao.com>
2025-02-05 10:10:41 +0900

    feat(capitalizing): ignore surrounding underscores

--- src/main/java/com/example/util/CapitalizingUtil.java
+++ src/main/java/com/example/util/CapitalizingUtil.java
@@ -5,29 +5,41 @@ public final class CapitalizingUtil {
     private CapitalizingUtil() {}
 
     // UPPER_CASE 문자열 -> 그중에서 A-Z, a-z, _ 구간 및 그 주변만 인식해서 Capitalizing 하는 메서드
-    public static String capitalize(String str) {
-        char[] origin = str.toCharArray();
-        char[] target = new char[origin.length];
+    public static String capitalize(String origin) {
+        if (origin == null || origin.isBlank()) {
+            return origin;
+        }
 
-        target[0] = toUpperCase(origin[0]);
-        for (int i = 1; i < origin.length; i++) {
-            if (origin[i] == '_') {
-                target[i] = ' ';
+        StringBuilder builder = new StringBuilder(origin.length());
+        boolean isPreviousBlank = true;
+
+        for (int i = 0; i < origin.length(); i++) {
+            char ch = origin.charAt(i);
+
+            // underscores, hyphens to blank
+            if (ch == '_' || ch == '-' || Character.isWhitespace(ch)) {
+                if (isPreviousBlank) {
+                    continue;
+                }
+                builder.append(' ');
+                isPreviousBlank = true;
                 continue;
             }
 
-            if (origin[i - 1] == '_') {
-                target[i] = toUpperCase(origin[i]);
-            } else {
-                target[i] = toLowerCase(origin[i]);
+            // Each first character of seperated words => to uppercase, others => to lowercase
+            if ('A' <= ch && ch <= 'Z' || 'a' <= ch && ch <= 'z') { // priority: && > ||
+                char convertedChar = isPreviousBlank ? toUpperCase(ch) : toLowerCase(ch);
+                builder.append(convertedChar);
             }
+
+            isPreviousBlank = false;
         }
 
-        return String.valueOf(target);
+        return builder.toString();
     }
 
     private static char toUpperCase(char ch) {
-        return (char) (ch & ~0x20);
+        return (char) (ch & ~' ');
     }
 
     private static char toLowerCase(char ch) {
